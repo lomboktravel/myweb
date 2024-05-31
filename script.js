9b8e9f3e69cb751cdc19f74f0511957def4a1495
lomboktravel <masyhur54@gmail.com>
2024-06-01 02:29:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your message!');
+    // You can add code here to send the form data to a server if needed.
+});
